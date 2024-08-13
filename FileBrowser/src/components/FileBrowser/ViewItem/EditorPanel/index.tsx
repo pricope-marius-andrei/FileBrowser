3c2998a83b9e2a6ec5b58ea567c8e32e313a2292
pricope-marius-andrei <pricopemarius33@gmail.com>
2024-08-13 07:42:03 +0300

    create save changes component

--- FileBrowser/src/components/FileBrowser/ViewItem/EditorPanel/index.tsx
+++ FileBrowser/src/components/FileBrowser/ViewItem/EditorPanel/index.tsx
@@ -1,25 +1,53 @@
-import { TabPanel, Textarea } from "@headlessui/react"
+import { Button, Description, Dialog, DialogBackdrop, DialogPanel, DialogTitle, TabPanel, Textarea } from "@headlessui/react"
 import { useContext, useEffect, useState } from "react"
 import { useDispatch } from "react-redux";
 import { updateItem } from "../../../../state/fileBrowserSlice";
 import { FileBrowserContext } from "../../../../contexts/fileBrowserContext";
+import { SaveChangesDialog } from "../SaveChangesDialog";
 
 export const EditorPanel = () => {
     const dispatch = useDispatch();
     const {activePath, currentItem, setCurrentItem} = useContext(FileBrowserContext);
+    const currentSavedValue = currentItem?.content;
     const [editorValue, setEditorValue] = useState(currentItem?.content);
+
+    const [dialogIsOpen, setDialogIsOpen] = useState(false);
+    const [isSaved, setIsSaved] = useState(false);
 
     useEffect(()=> {
       setEditorValue(currentItem?.content)
     }, [activePath])
+
+    useEffect(()=> {
+      setIsSaved(editorValue === currentItem?.content);
+    },[editorValue])
+
+    const handleSaveChanges = () => {
+      setDialogIsOpen(false);  dispatch(updateItem({activePath, newContent: editorValue}));
+      setCurrentItem({...currentItem, content: editorValue});
+      setIsSaved(true);
+    }
+
+    const handleCancelChanges = () => {
+      setDialogIsOpen(false)
+      setEditorValue(currentSavedValue);
+    }
     
   return (
-    <TabPanel className="flex grow">
-        <Textarea onBlur={()=>{
-          dispatch(updateItem({activePath, newContent: editorValue}));
-          setCurrentItem({...currentItem, content: editorValue});
-          
-          }} autoFocus={true} onChange={(textarea)=> setEditorValue(textarea.target.value)} value={editorValue} className="p-2 m-10 h-48 grow bg-slate-800"></Textarea>
-    </TabPanel>
+    <>
+      {!isSaved && <p>The file was modified</p>}
+      <TabPanel className="flex grow">
+          <Textarea 
+          onBlur={()=>{
+              if(!isSaved)
+              setDialogIsOpen(true);
+            }} autoFocus={true} onChange={(textarea)=> setEditorValue(textarea.target.value)} value={editorValue} className="p-2 m-10 h-48 grow bg-slate-800"></Textarea>
+      </TabPanel>
+     <SaveChangesDialog 
+        dialogIsOpen={dialogIsOpen} 
+        setDialogIsOpen={setDialogIsOpen} 
+        handleCancelChanges={handleCancelChanges} 
+        handleSaveChanges={handleSaveChanges}/>
+    </>
   )
 }
